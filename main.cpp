9edfcf539a3d2d57d99f605dfbb2b7bd77728655
Theppaith N <tutorgaming@gmail.com>
2015-10-04 20:10:26 +0700

    Dragable Crop Region - Detector Text - MouseEvent implemented - KNN Ratio Tune

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 /************************************************
 Name : CV_Playground
-Version : 3
+Version : 3-4
 Detail : SIFT Algorithm with KNN Implemented
 Author : Theppasith N.
 28-AUG-2015 08:00AM
+4-OCT-2015 02:00AM
+REF :
+http://stackoverflow.com/questions/22722772/how-to-use-sift-in-opencv
+http://stackoverflow.com/questions/17967950/improve-matching-of-feature-points-with-opencv
+http://stackoverflow.com/questions/23440694/heap-corruption-exception-in-knnmatch-opencv
+http://morf.lv/modules.php?name=tutorials&lasit=2
+http://www.programering.com/a/MTNzAzMwATc.html
+REF : (Mouse Event)
+https://jayrambhia.wordpress.com/2012/09/20/roi-bounding-box-selection-of-mat-images-in-opencv/
 ************************************************/
 #include <iostream>
 #include <opencv2/opencv.hpp>
@@ -13,33 +22,92 @@ Author : Theppasith N.
 
 using namespace std;
 using namespace cv;
+cv::Point firstClick,secondClick;
+Mat capturedImage,capturedSelection;
+Rect selection;
+bool dragflag = false;
+bool finished = false;
 
-Mat capturedImage;
+void myMouseEvent(int event , int x ,int y , int flags , void* param ){
+    if(event == CV_EVENT_LBUTTONDOWN && !dragflag){
+        //Left Mouse Click => Set State to clicked
+        firstClick = Point(x,y);
+        dragflag = true;
+    }
+    if(event == CV_EVENT_MOUSEMOVE && dragflag){
+        //Determine the end point of rectangle and Show Drawing on screen
+        Mat img1 = capturedImage.clone(); //Mock Up for Visualizing
+        secondClick = Point(x,y);
+        rectangle(img1,firstClick,secondClick,CV_RGB(0,255,0),3,8,0); //Draw on Visualizer
+        imshow("crop",img1); //Display Visualizer instead of real mat
+    }
+    if(event == CV_EVENT_LBUTTONUP && dragflag){
+        //Mouse Release => Get the Selected Area to CaptureSelection
+        secondClick = Point(x,y);
+        selection = Rect(firstClick.x,firstClick.y,abs(firstClick.x - secondClick.x),abs(firstClick.y-secondClick.y)); //Size of Selected Area
+        dragflag = false;
+        capturedSelection = capturedImage(selection);
+    }
+    if(event == CV_EVENT_LBUTTONUP){
+        //State For Mouse Click but no drag
+        finished = true;
+        dragflag = false;
+    }
+}
 
-//int captureImg(){
-//    VideoCapture camera(0);
-//    char x;
-//    while(1){
-//        x = waitKey(10);
-//        camera >> capturedImage;
-//        imshow("capture",capturedImage);
-//        if( x=='s'){
-//            return 0;
-//        }
-//    }
-//}
+
+int captureImg(){
+    VideoCapture camera(0);
+    namedWindow("PRESS S TO CAPTURE");
+    char keyboardInput;
+    while(1){
+        keyboardInput = waitKey(10);
+        camera.read(capturedImage);
+        imshow("PRESS S TO CAPTURE",capturedImage);
+        if( keyboardInput=='s'){
+            destroyWindow("PRESS S TO CAPTURE");
+            return 0;
+        }
+    }
+}
+
+int cropSelection(){
+    namedWindow("crop");
+    while(1){
+       cv::waitKey(10);
+        imshow("crop",capturedImage);
+        cvSetMouseCallback("crop", myMouseEvent, NULL);
+        if(finished && !capturedSelection.empty()){
+            cout << "Cropping finished" <<endl;
+            destroyWindow("crop");
+            //namedWindow("cropped");
+            //imshow("cropped",capturedSelection);
+            return 0;
+        }
+    }
+}
+
 
 int main(){
-    //Object Image
-    Mat inputImage,vidStream;                             //Input Image
-    inputImage = imread("loft.jpg");            //Load Image
-    ////////////////////////////////////////
-    //captureImg();
+    //Mat For Image
+    Mat inputImage,vidStream;                     //Input Image
+    int manuallySelect = 0;
+    cout << "0 = Load JPG" << endl << "1 = Capture from Device" <<endl << " = " ;
+    cin >> manuallySelect ;
+    //Load Image
+    if(!manuallySelect){
+        inputImage = imread("loft.jpg");
+    }else{
+        captureImg();
+        cropSelection();
+        inputImage = capturedSelection;
+    }
     //Convert To Gray => to SIFT
     if(inputImage.empty()){
-        std::cout << "No Such File" <<std::endl;
+        std::cout << "No Such INPUT" <<std::endl;
         return -1;
     }
+
         Mat inputGray;
         cv::cvtColor(inputImage , inputGray , CV_BGR2GRAY);
     //SOURCE Object SIFT Keypoint Detector
@@ -77,7 +145,7 @@ int main(){
         //vector<vector<cv::DMatch> > matches;
         //cout << keypoints.size() <<endl;
 
-        float thresholdMatchingNN=0.7;
+        float thresholdMatchingNN=0.8;
        //unsigned int thresholdGoodMatches=10;
 
 
@@ -100,6 +168,12 @@ int main(){
                         && ((int) matches[i].size()<=2 && (int) matches[i].size()>0)){
                         good_matches.push_back(matches[i][0]);
                     }
+                }
+                //cout << "Amount is = " << good_matches.size() <<endl;
+                if(good_matches.size() > 120){
+                    cout <<"@@@@@@[" << good_matches.size() << "]Detected Object" <<endl;
+                }else{
+                    cout <<"[" << good_matches.size() << "]No Detected" <<endl;
                 }
             //Originally
             /*
